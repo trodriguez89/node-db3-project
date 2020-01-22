f48c7a1f0e390df2746800531d4e8e284d1a4834
trodriguez89 <trodriguez89@gmail.com>
2020-01-22 15:14:13 -0800

    mvp finished

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,16 +1,57 @@
 const db = require("../data/dbConfig");
 
 module.exports = {
-
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+  addStep
 }
 
 function find() {
-    return db("schemes");
+  return db("schemes");
 };
 
-function findById(id){
-    return db("schemes")
+function findById(id) {
+  return db("schemes")
     .where({ id })
     .first();
-}
+};
+
+function findSteps(id) {
+  return db("steps")
+    .join("schemes", "steps.scheme_id", "schemes.id")
+    .select("steps.id", "schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .where("schemes.id", id)
+    .orderBy("steps.step_number")
+};
+
+function add(schemeInfo) {
+  return db("schemes")
+    .insert(schemeInfo)
+    .then(ids => {
+      return findById(ids[0])
+    })
+};
+
+function addStep(stepData, id) {
+  return db("steps")
+    .insert({ step_number: stepData.step_number, instructions: stepData.instructions, scheme_id: id })
+};
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes)
+};
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del();
+};
+
+
 
